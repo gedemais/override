8fce6c4eff6dbef9ccc7bf0c9f1310a07daa7659
morgan guy <morganguy@morgans-MacBook-Pro.local>
2022-02-04 19:57:25 +0100

    level4 source finished

---
+++ level04/source.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+int main()
+{
+    int     tmp;
+    int     status;
+    char    buff[32];
+    pid_t   addr;
+
+    addr = fork();
+    memset(buff, 32, 0);
+    tmp = 0;
+    if (addr == 0)
+    {
+        prctl(1, 1);
+        ptrace(0, 0, 0, 0);
+        puts("Give me some shellcode, k");
+        gets(buff);
+    }
+    else
+    {
+        while (ptrace(3, addr, 44, 0) != 0xb)
+        {
+            wait(&status);
+            if ((tmp & 0x7f) == 0 || ((tmp & 0x7f) + 1) >> 1 > 0)
+            {
+                puts("child is exiting...");
+                return (0);
+            }
+        }
+        puts("no exec() for you");
+        kill(addr, 9);
+    }
+    return (0);
+}
