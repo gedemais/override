43ee41c043c482246eb619a29aee7987511e5ccc
morgan guy <morganguy@morgans-MacBook-Pro.local>
2022-02-06 10:44:06 +0100

    walkthrough level06 finished but needs some explanation and source finalized but ugly

--- level06/source.c
+++ level06/source.c
@@ -1,19 +1,55 @@
 #include <stdio.h>
 #include <string.h>
 
-int		auth(char *buffer, int serial)
+int		auth(char *buff, int serial)
 {
-	buffer += strcspn(buffer, "\n");
-	buffer[0] = 0;
+	char	c;   //-0x10(ebp)
+	int		tmp;
+	int		tmp2;
+	int		tmp3;
+	int		i; //-0x14(ebp)
+	int		len; //-0xc(ebp)
 
-	strnlen(buffer, 32);
-	return (0);
+	buff[strcspn(buff, "\n") - 1] = 0;
+	len = strnlen(buff, 32);
+	if (len != 0)
+		len += 4;
+	if (len > 5)
+		return (1);
+	if (ptrace(0, 0, 1, 0) == -1)
+	{
+		puts("\033[32m.---------------------------.");
+		puts("\033[31m| !! TAMPERING DETECTED !!  |");
+		puts("\033[32m'---------------------------'");
+	}
+	else
+	{
+		c = (buff[2] ^ 0x1337) + 0x5eeded;
+		i = 0;
+		while (len > i)
+		{
+			if (buff[i] < 0x1f)
+				return (1);
+			tmp = c ^ buff[i];
+			tmp2 = tmp * 0x88233b2b;
+			tmp3 -= 0x88233b2b;
+			tmp3 = tmp >> 1;
+			tmp3 += 0x88233b2b;
+			tmp3 = tmp * 0x539;
+			tmp -= tmp3;
+			c += tmp;
+			i++;
+		}
+		if (serial == c)
+			return (0);
+	}
+	return (1);
 }
 
 int		main(void)
 {
 	int		serial; // 0x28
-	char	buffer[32]; // 0x2c
+	char	buff[32]; // 0x2c
 
 	puts("***********************************");
 	puts("*		level06		  *");
@@ -29,7 +65,7 @@ int		main(void)
 	printf("-> Enter Serial: ");
 	scanf("%d", &serial);
 
-	if (auth(buffer, serial) == 0)
+	if (auth(buff, serial) == 0)
 	{
 		puts("Authenticated!");
 		system("/bin/sh");
