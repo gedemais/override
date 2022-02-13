89eb746555488fc6f2c1701b14c4aec40986be24
morgan guy <morganguy@morgans-MacBook-Pro.local>
2022-02-13 20:22:39 +0100

    walkthrough level08 finished

--- level08/source.c
+++ level08/source.c
@@ -4,8 +4,8 @@ void    log_wrapper(int fd, char *log, char *input) // -0x118(%rbp), -0x120(%rbp
 
     strcpy(buff, log);
     snprintf(*buff + strlen(buff), 254 - strlen(buff), input);
-    buff[strcspn(buff, 0x400d4c)] = 0;
-    fprintf(fd, 0x400d4e, buff);
+    buff[strcspn(buff, "\n")] = 0; // 0x400d4c
+    fprintf(fd, "LOG: %s\n", buff); // 0x400d4e
 
     return (0);
 }
@@ -15,6 +15,7 @@ int         main(int ac, char **av)
     int     fd; // -0x88(%rbp)
     int     fd2; // -0x80(%rbp)
     int     fd3 = -1; // -0x78(%rbp)
+    char    tmp;
     char    buff[104] = {0};
 
     if (ac != 2) // $0x2,-0x94(%rbp)
@@ -29,15 +30,16 @@ int         main(int ac, char **av)
     }
 
     log_wrapper(fd, "Starting back up: ", av[1]); // 0x400d96
-    fd2 = fopen(av[2], 'r'); // 0x400da9
+    fd2 = fopen(av[1], 'r'); // 0x400da9
 
     if (fd2 == 0)
     {
-        printf("ERROR: Failed to open %s\n", av[2]); // 0x400d7c
+        printf("ERROR: Failed to open %s\n", av[1]); // 0x400d7c
         exit(1);
     }
 
-    strncat(buff, av[2], 99 - strlen(buff));
+    memcpy(buff, "./backups/", 10); // 0x400dab
+    strncat(buff, av[1], 99 - strlen(buff));
     fd3 = open(buff, 0xc1, 0x1b0);
 
     if (fd3 < 0)
@@ -46,10 +48,8 @@ int         main(int ac, char **av)
         exit(1);
     }
     
-    while (fgetc(fd2) != 255)
-    {
-        write(fd3, 255, 1);
-    }
+    while ((tmp = fgetc(fd2)) != 0xff)
+        write(fd3, tmp, 1);
 
     log_wrapper(fd, "Finished back up ", av[1]); // 0x400dd2
     fclose(fd2);
