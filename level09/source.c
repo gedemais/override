376ffe59fe4f18fe354dad10c5b353f2eccdf9c0
morgan guy <morganguy@morgans-MacBook-Pro.local>
2022-02-13 21:03:18 +0100

    source level09 finished

--- level09/source.c
+++ level09/source.c
@@ -1,4 +1,6 @@
-int	secret_backdoor(void)
+#include <stdio.h>
+
+int			secret_backdoor(void)
 {
 	char	buffer[128];
 
@@ -7,17 +9,35 @@ int	secret_backdoor(void)
 	return (0);
 }
 
-int	set_username(char *username)
+void		set_username(char *username)
 {
+	int		i = 0;
 	char	buffer[144];
 
 	memset(buffer, 0, 10);
 	puts(">: Enter your username");
 	printf(">>: ");
 	fgets(buffer, 128, stdin);
+	while (i < 40 && buffer[i] != 0)
+	{
+		*(username + 0x8c + i) = buffer[i];
+		i++;
+	}
+	printf("", *(username + 0x8c)); // 0x165(%rip) 0xbfb
 }
 
-int	handle_msg(void)
+void 		set_msg(char *username)
+{
+	char	buff[1024];
+
+	memset(buff, 0, 128);
+	puts(""); //0x265(%rip) 0xbcd
+	printf(""); //0x26b(%rip) 0xbdf
+	fgets(buff, 1024, ""); // 0x201630(%rip) 0x201fb8
+	strncpy(username, buff, *username+0xb4);
+}
+
+int			handle_msg(void)
 {
 	long	n;
 	char	username[180];
@@ -31,7 +51,7 @@ int	handle_msg(void)
 	return (0);
 }
 
-int	main(void)
+int		main(void)
 {
 	puts("--------------------------------------------\n|   ~Welcome to l33t-m$n ~    v1337        |\n--------------------------------------------");
 	handle_msg();
