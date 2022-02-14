0661649e93fb24a2484db9ed8c876ee4062518e8
morgan guy <morganguy@morgans-MacBook-Pro.local>
2022-02-14 13:21:35 +0100

    walkthrough and source level09 finished

--- level09/source.c
+++ level09/source.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
+typedef struct s_user
+{
+	char	username[180];
+	long	n;
+} t_user;
+
 int			secret_backdoor(void)
 {
 	char	buffer[128];
 
 	fgets(buffer, 128, stdin);
 	system(buffer);
+
 	return (0);
 }
 
-void		set_username(char *username)
+void		set_username(t_user *user)
 {
 	int		i = 0;
 	char	buffer[144];
@@ -18,15 +25,17 @@ void		set_username(char *username)
 	puts(">: Enter your username");
 	printf(">>: ");
 	fgets(buffer, 128, stdin);
+
 	while (i < 40 && buffer[i] != 0)
 	{
-		*(username + 0x8c + i) = buffer[i];
+		*(user->username + 0x8c + i) = buffer[i];
 		i++;
 	}
-	printf("", *(username + 0x8c)); // 0x165(%rip) 0xbfb
+	
+	printf("", *(user->username + 0x8c)); // 0x165(%rip) 0xbfb
 }
 
-void 		set_msg(char *username)
+void 		set_msg(t_user *user)
 {
 	char	buff[1024];
 
@@ -34,18 +43,17 @@ void 		set_msg(char *username)
 	puts(">: Msg @Unix-Dude"); //0x265(%rip) 0xbcd
 	printf(">>: "); //0x26b(%rip) 0xbdf
 	fgets(buff, 1024, stdin); // 0x201630(%rip) 0x201fb8
-	strncpy(username, buff, *(username + 0xb4));
+	strncpy(user->username, buff, user->n);
 }
 
 int			handle_msg(void)
 {
-	long	n;
-	char	username[180];
+	t_user 	user;
 
-	memset(username, 0, 40);
-	n = 140;
-	set_username(username);
-	set_msg(username);
+	memset(user.username+0x8c, 0, 40);
+	user.n = 140;
+	set_username(&user);
+	set_msg(&user);
 	puts(">: Msg sent!");
 
 	return (0);
@@ -55,5 +63,6 @@ int		main(void)
 {
 	puts("--------------------------------------------\n|   ~Welcome to l33t-m$n ~    v1337        |\n--------------------------------------------");
 	handle_msg();
+
 	return (0);
 }
